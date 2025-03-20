b2be5a75c5dcf8ca9eb57833181acef2f65ddd46
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-03-20 11:30:23 -0300

    Create 3048_terminar.java

---
+++ 3048_terminar.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int Ncontagem;
+        int[] lista = new int[N];
+        int N=sc.nextInt();
+        for( Math.abs(N); 3<=N && N<=500 ; NC++){
+            lista[n]=sc.nextInt();
+            if (N<3 && N>500) {
+            break;
+        }
+        if (3<=N && N<=500) {
+            if (lista[0]!=lista[1]){
+            Ncontagem++;
+            
+            }
+        }
+
+        }
+    }
+}
