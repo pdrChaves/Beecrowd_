ef0889357f88e0221800c6e337352f8d1b6da97d
Pedro Chaves <pedroa.chaves10@gmail.com>
2025-04-03 08:36:36 -0300

    Create 1179 terminar.java

---
+++ Java/1179 terminar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int [15] x = new int[15];
+        int [5] par = new int [5];
+        int [5] impar = new int [5];
+        int N;
+        while (N) {
+            int N= sc.nextInt();
+            
+            
+        }
+    }
+} 
